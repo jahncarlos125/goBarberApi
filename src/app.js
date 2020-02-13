abeb95b7b208d1fe49600602e53f4bfdd3e9407c
Jahn Carlos <jahn.santana@gmail.com>
2020-02-13 20:09:31 -0300

    Configure prettier and eslint

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import express from "express";
-import routes from "./routes";
+import express from 'express';
+import routes from './routes';
 
 class App {
   constructor() {
@@ -11,6 +11,7 @@ class App {
   middlewares() {
     this.server.use(express.json());
   }
+
   routes() {
     this.server.use(routes);
   }
