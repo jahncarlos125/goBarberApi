7e108c5dbe8686ae235ee2a8d6e92e88ff6bef60
Jahn Carlos <jahn.santana@gmail.com>
2020-02-14 21:14:45 -0300

    list schedule

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -8,6 +8,7 @@ import Appointment from '../models/Appointment';
 class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
