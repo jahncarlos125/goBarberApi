9fe5d3d2779545b57776cebe43c3665f466048d1
Jahn Carlos <jahn.santana@gmail.com>
2020-02-15 13:28:40 -0300

    Add send email to canceled appointment

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'bddd9cf06fbcb5',
+    pass: '487d7b87944b08',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
